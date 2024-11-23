a0879d4534732e52b4983d96bd0fd4212270b509
Ricardo Pinto <rgb2frame@gmail.com>
2024-11-23 00:35:59 +0000

    Description of changes made

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
-void main() => runApp (
+
+void main() => runApp(
   const MaterialApp(
     debugShowCheckedModeBanner: false,
     home: Homepage(),
-  )
+  ),
 );
 
 class Homepage extends StatelessWidget {
@@ -11,8 +12,12 @@ class Homepage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // Obter a altura da tela
+    double screenHeight = MediaQuery.of(context).size.height;
+
     return Scaffold(
-      body: Container(
+      body: SingleChildScrollView(  // Adicionando o SingleChildScrollView
+        child: Container(
           padding: const EdgeInsets.symmetric(vertical: 30),
           width: double.infinity,
           decoration: const BoxDecoration(
@@ -21,109 +26,142 @@ class Homepage extends StatelessWidget {
               fit: BoxFit.cover,
             ),
           ),
-        
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            const SizedBox(height: 80,),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              const SizedBox(height: 80,),
 
-            const Padding(
+              const Padding(
                 padding: EdgeInsets.all(20),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text("Login", style: TextStyle(color: Colors.white, fontSize: 40),),
-                  Text("Bem vindos á Rua Saudavel", style: TextStyle(color: Colors.white, fontSize: 20),)
-                ],
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text("Login", style: TextStyle(color: Colors.black, fontSize: 40),),
+                    Text("Bem vindos á Rua Saudavel", style: TextStyle(color: Colors.black, fontSize: 20),),
+                  ],
+                ),
               ),
-            ),
 
-            const SizedBox(height: 20,),
+              const SizedBox(height: 20,),
 
-            Expanded(
-                child: Container(
-                  decoration: const BoxDecoration(
-                      color: Color.fromRGBO(255, 253, 208, 20),
+              // Container agora estará dentro do SingleChildScrollView, então ele poderá rolar
+              Container(
+                height: screenHeight - 80 - 40, // Subtrai o espaço do topo e padding
+                decoration: const BoxDecoration(
+                    color: Color.fromRGBO(255, 253, 208, 20),
                     borderRadius: BorderRadius.only(
                         topLeft: Radius.circular(60),
                         topRight: Radius.circular(60)
                     )
-                  ),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(30),
+                  child: Column(
+                    children: <Widget>[
+                      const SizedBox(height: 60,), //entre "Bem vindos" e a Box Preta
 
-                  child: Padding(
-                    padding: const EdgeInsets.all(30),
-                    child: Column(
-                      children: <Widget>[
-
-                        const SizedBox( height: 60,), //entre "Bem vindos" e a Box Preta
-
-                        Container(
-
-                          decoration: BoxDecoration(
-
+                      Container(
+                        decoration: BoxDecoration(
                             color: Colors.white,
-
                             borderRadius: BorderRadius.circular(10),
-
                             boxShadow: const [BoxShadow(
-                              color: Color.fromRGBO(225, 95, 27, .3),
-                              blurRadius: 20,
-                              offset: Offset(0, 10)
+                                color: Color.fromRGBO(225, 95, 27, .3),
+                                blurRadius: 20,
+                                offset: Offset(0, 10)
                             )]
-                          ),
-
-                          child: Column(
-                            children: <Widget>[
-
-                              //Container para o Local de escrever  os dados
-                              Container(
-
-                                padding: const EdgeInsets.all(10), //Tamanho da caixa do Email e Telemovel
-
-                                decoration: const BoxDecoration(
+                        ),
+                        child: Column(
+                          children: <Widget>[
+                            //Container para o Local de escrever os dados
+                            Container(
+                              padding: const EdgeInsets.all(10), //Tamanho da caixa do Email e Telemovel
+                              decoration: const BoxDecoration(
                                   border: Border(bottom: BorderSide(color: Colors.grey))
-                                ),
-
-                                child: const TextField(
-                                  decoration: InputDecoration(
+                              ),
+                              child: const TextField(
+                                decoration: InputDecoration(
                                     hintText: "Email ou Telemovel",
                                     hintStyle: TextStyle(color: Colors.grey),
                                     border: InputBorder.none
-                                  ),
                                 ),
                               ),
-
-                              Container(
-
-                                padding: const EdgeInsets.all(10), //Tamanho da caixa do Email e Telemovel
-
-                                decoration: const BoxDecoration(
-                                    border: Border(bottom: BorderSide(color: Colors.grey))
+                            ),
+                            Container(
+                              padding: const EdgeInsets.all(10), //Tamanho da caixa do Email e Telemovel
+                              decoration: const BoxDecoration(
+                                  border: Border(bottom: BorderSide(color: Colors.grey))
+                              ),
+                              child: const TextField(
+                                decoration: InputDecoration(
+                                    hintText: "Password",
+                                    hintStyle: TextStyle(color: Colors.grey),
+                                    border: InputBorder.none
                                 ),
-
-                                child: const TextField(
-                                  decoration: InputDecoration(
-                                      hintText: "Password",
-                                      hintStyle: TextStyle(color: Colors.grey),
-                                      border: InputBorder.none
-                                  ),
-                                ),
-                              )
-                            ],
-                          ),
+                              ),
+                            )
+                          ],
                         ),
-                        const SizedBox(height: 30,),
-                        const Text("Forgot Password", style: TextStyle(color: Colors.grey),)
+                      ),
+                      const SizedBox(height: 30,),
 
-                      ],
-                    ),
+                      const Text("Forgot Password", style: TextStyle(color: Colors.grey),),
+
+                      const SizedBox(height: 40,),
+
+                      Container(
+                        height: 50,
+                        margin: const EdgeInsets.symmetric(horizontal: 50),
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(50),
+                            color: Colors.blueGrey
+                        ),
+                        child: const Center(
+                          child: Text("Login", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
+                        ),
+                      ),
+                      const SizedBox(height: 30,),
+
+                      const Text("Continuar com outras plataformas", style: TextStyle(color: Colors.grey),),
+
+                      const SizedBox(height: 30,),
+
+                      Row(
+                        children: <Widget>[
+                          Expanded(
+                            child: Container(
+                              height: 50,
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(50),
+                                  color: Colors.blue
+                              ),
+                              child: const Center(
+                                child: Text("FaceBook", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
+                              ),
+                            ),
+                          ),
+                          const SizedBox(width: 50,),
+                          Expanded(
+                            child: Container(
+                              height: 50,
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(50),
+                                  color: Colors.black
+                              ),
+                              child: const Center(
+                                child: Text("Google", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ],
                   ),
                 ),
-            )
-          ],
-        )
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
 }
-
