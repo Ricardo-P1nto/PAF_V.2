c55ecd710d9c8c6a21d60459af68f5483209abb8
rgb2f <rgb2frame@gmail.com>
2024-11-28 22:27:35 +0000

    a

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'PaginaPrincipal.dart';
+
 void main() => runApp(
   const MaterialApp(
     debugShowCheckedModeBanner: false,
@@ -12,156 +14,238 @@ class Homepage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // Obter a altura da tela
-    double screenHeight = MediaQuery.of(context).size.height;
-
     return Scaffold(
-      body: SingleChildScrollView(  // Adicionando o SingleChildScrollView
-        child: Container(
-          padding: const EdgeInsets.symmetric(vertical: 30),
-          width: double.infinity,
-          decoration: const BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage("assets/imagens/FotoDeLisboa.jpg"), // Coloque o caminho da sua imagem
-              fit: BoxFit.cover,
-            ),
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              const SizedBox(height: 80,),
-
-              const Padding(
-                padding: EdgeInsets.all(20),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text("Login", style: TextStyle(color: Colors.black, fontSize: 40),),
-                    Text("Bem vindos á Rua Saudavel", style: TextStyle(color: Colors.black, fontSize: 20),),
-                  ],
+      body: SafeArea( // Envolve todo o conteúdo para evitar áreas inseguras
+        child: Stack(
+          children: [
+            // Imagem de fundo
+            Container(
+              width: double.infinity,
+              height: double.infinity,
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage("assets/imagens/FotoDeLisboa2.jpg"),
+                  fit: BoxFit.cover, // Cobre toda a tela sem distorcer
                 ),
               ),
+            ),
 
-              const SizedBox(height: 20,),
+            // Conteúdo principal
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(height: 70), // Espaço superior
 
-              // Container agora estará dentro do SingleChildScrollView, então ele poderá rolar
-              Container(
-                height: screenHeight - 80 - 40, // Subtrai o espaço do topo e padding
-                decoration: const BoxDecoration(
-                    color: Color.fromRGBO(255, 253, 208, 20),
-                    borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(60),
-                        topRight: Radius.circular(60)
-                    )
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(30),
+                const Padding(
+                  padding: EdgeInsets.all(20),
                   child: Column(
-                    children: <Widget>[
-                      const SizedBox(height: 60,), //entre "Bem vindos" e a Box Preta
-
-                      Container(
-                        decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(10),
-                            boxShadow: const [BoxShadow(
-                                color: Color.fromRGBO(225, 95, 27, .3),
-                                blurRadius: 20,
-                                offset: Offset(0, 10)
-                            )]
-                        ),
-                        child: Column(
-                          children: <Widget>[
-                            //Container para o Local de escrever os dados
-                            Container(
-                              padding: const EdgeInsets.all(10), //Tamanho da caixa do Email e Telemovel
-                              decoration: const BoxDecoration(
-                                  border: Border(bottom: BorderSide(color: Colors.grey))
-                              ),
-                              child: const TextField(
-                                decoration: InputDecoration(
-                                    hintText: "Email ou Telemovel",
-                                    hintStyle: TextStyle(color: Colors.grey),
-                                    border: InputBorder.none
-                                ),
-                              ),
-                            ),
-                            Container(
-                              padding: const EdgeInsets.all(10), //Tamanho da caixa do Email e Telemovel
-                              decoration: const BoxDecoration(
-                                  border: Border(bottom: BorderSide(color: Colors.grey))
-                              ),
-                              child: const TextField(
-                                decoration: InputDecoration(
-                                    hintText: "Password",
-                                    hintStyle: TextStyle(color: Colors.grey),
-                                    border: InputBorder.none
-                                ),
-                              ),
-                            )
-                          ],
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Login",
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 40,
                         ),
                       ),
-                      const SizedBox(height: 30,),
-
-                      const Text("Forgot Password", style: TextStyle(color: Colors.grey),),
-
-                      const SizedBox(height: 40,),
-
-                      Container(
-                        height: 50,
-                        margin: const EdgeInsets.symmetric(horizontal: 50),
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(50),
-                            color: Colors.blueGrey
+                      Text(
+                        "Bem vindos à Rua Saudável",
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 20,
                         ),
-                        child: const Center(
-                          child: Text("Login", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
-                        ),
-                      ),
-                      const SizedBox(height: 30,),
-
-                      const Text("Continuar com outras plataformas", style: TextStyle(color: Colors.grey),),
-
-                      const SizedBox(height: 30,),
-
-                      Row(
-                        children: <Widget>[
-                          Expanded(
-                            child: Container(
-                              height: 50,
-                              decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(50),
-                                  color: Colors.blue
-                              ),
-                              child: const Center(
-                                child: Text("FaceBook", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
-                              ),
-                            ),
-                          ),
-                          const SizedBox(width: 50,),
-                          Expanded(
-                            child: Container(
-                              height: 50,
-                              decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(50),
-                                  color: Colors.black
-                              ),
-                              child: const Center(
-                                child: Text("Google", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
-                              ),
-                            ),
-                          )
-                        ],
                       ),
                     ],
                   ),
                 ),
-              ),
-            ],
-          ),
+
+                const Spacer(), // Empurra o container amarelo para baixo
+
+                // Container amarelo com campos de login
+                Expanded(
+                  child: SingleChildScrollView( // Permite rolagem caso necessário
+                    child: Container(
+                      width: double.infinity,
+                      decoration: const BoxDecoration(
+                        color: Color.fromRGBO(255, 253, 208, 1),
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(60),
+                          topRight: Radius.circular(60),
+                        ),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(30),
+                        child: Column(
+                          children: [
+                            const SizedBox(height: 30), // Espaçamento entre título e campos
+
+                            // Campos de texto
+                            Container(
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(10),
+                                boxShadow: const [
+                                  BoxShadow(
+                                    color: Color.fromRGBO(225, 95, 27, .3),
+                                    blurRadius: 20,
+                                    offset: Offset(0, 10),
+                                  ),
+                                ],
+                              ),
+                              child: Column(
+                                children: [
+                                  Container(
+                                    padding: const EdgeInsets.all(10),
+                                    decoration: const BoxDecoration(
+                                      border: Border(
+                                        bottom: BorderSide(color: Colors.grey),
+                                      ),
+                                    ),
+                                    child: const TextField(
+                                      decoration: InputDecoration(
+                                        hintText: "Email ou Telemóvel",
+                                        hintStyle: TextStyle(color: Colors.grey),
+                                        border: InputBorder.none,
+                                      ),
+                                    ),
+                                  ),
+                                  Container(
+                                    padding: const EdgeInsets.all(10),
+                                    child: const TextField(
+                                      obscureText: true,
+                                      decoration: InputDecoration(
+                                        hintText: "Password",
+                                        hintStyle: TextStyle(color: Colors.grey),
+                                        border: InputBorder.none,
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+
+                            const SizedBox(height: 20),
+
+                            const Text(
+                              "Forgot Password",
+                              style: TextStyle(color: Colors.grey),
+                            ),
+
+                            const SizedBox(height: 20),
+
+                            // Botão de Login
+                            Container(
+                              height: 50,
+                              margin: const EdgeInsets.symmetric(horizontal: 50),
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(50),
+                                color: Colors.blueGrey,
+                              ),
+                              child: const Center(
+                                child: Text(
+                                  "Login",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                            ),
+
+                            const SizedBox(height: 5),
+
+                            // Botão de Criar Conta
+                            GestureDetector(
+                              onTap: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => const PaginaPrincipal(), // Redireciona para PaginaPrincipal
+                                  ),
+                                );
+                              },
+                              child: Container(
+                                height: 50,
+                                margin: const EdgeInsets.symmetric(horizontal: 50),
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(50),
+                                  color: Colors.blueGrey,
+                                ),
+                                child: const Center(
+                                  child: Text(
+                                    "Criar Conta",
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+
+                            const SizedBox(height: 20),
+
+                            const Text(
+                              "Continuar com outras plataformas",
+                              style: TextStyle(color: Colors.grey),
+                            ),
+
+                            const SizedBox(height: 20),
+
+                            // Botões de Facebook e Google
+                            Row(
+                              children: [
+                                Expanded(
+                                  child: Container(
+                                    height: 50,
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(50),
+                                      color: Colors.blue,
+                                    ),
+                                    child: const Center(
+                                      child: Text(
+                                        "Facebook",
+                                        style: TextStyle(
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                const SizedBox(width: 50),
+                                Expanded(
+                                  child: Container(
+                                    height: 50,
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(50),
+                                      color: Colors.black,
+                                    ),
+                                    child: const Center(
+                                      child: Text(
+                                        "Google",
+                                        style: TextStyle(
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
   }
 }
+
