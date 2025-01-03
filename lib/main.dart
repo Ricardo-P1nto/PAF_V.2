78abe2977a073ef071d466aa494ddae42c478967
rgb2f <rgb2frame@gmail.com>
2025-01-03 15:14:07 +0000

    Inicializando o projeto Flutter com .gitignore para arquivos sensíveis

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
 import 'PaginaRegistar.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   // Inicia o app
   runApp(const MyApp());
 }
