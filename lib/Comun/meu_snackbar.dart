930822c42f71bcdbc340b26573be4b569ec2816c
rgb2f <rgb2frame@gmail.com>
2025-01-29 11:12:08 +0000

    Inicializando o projeto Flutter com .gitignore para arquivos sensíveis

--- lib/Comun/meu_snackbar.dart
+++ lib/Comun/meu_snackbar.dart
@@ -1,8 +1,30 @@
 import 'package:flutter/material.dart';
 
-mostarSnackBar({required BuildContext context, required String mensagem, bool erro = true}) {
-  SnackBar snackBar = SnackBar(
+void mostarSnackBar({
+  required BuildContext context,
+  required String mensagem,
+  bool erro = true,
+}) {
+  final snackBar = SnackBar(
     content: Text(mensagem),
+    backgroundColor: erro ? Colors.red.shade900 : Colors.green.shade900,
+    behavior: SnackBarBehavior.floating,
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(10),
+    ),
+    margin: const EdgeInsets.only(
+      bottom: 10, // Add space between the SnackBar and the bottom of the screen
+      left: 10,
+      right: 10,
+    ),
+    duration: const Duration(seconds: 3),
+    action: SnackBarAction(
+      label: 'OK',
+      textColor: Colors.white,
+      onPressed: () {
+        ScaffoldMessenger.of(context).hideCurrentSnackBar();
+      },
+    ),
   );
 
   ScaffoldMessenger.of(context).showSnackBar(snackBar);
